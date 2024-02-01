729d6e6ebc10d2a3eb05dace472556f067516735
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-02-01 11:08:33 +0530

    Feed data to database 💉

---
+++ actions/feed-data.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+import { db } from "@/lib/db";
+
+interface Beer {
+    name: string;
+    image_url: string;
+}
+
+export const feedData = async () => {
+    try {
+        const apiUrl = `https://api.punkapi.com/v2/beers?page=1&per_page=24`;
+        const { data } = await axios.get<Beer[]>(apiUrl);
+
+        if (data && data.length > 0) {
+            const filteredData: Beer[] = data.map(item => ({
+                name: item.name,
+                image_url: item.image_url,
+            }));
+            if (filteredData && filteredData.length > 0) {
+                const res = await db.product.createMany({
+                    data: filteredData
+                })
+                return res;
+            }
+        }
+    } catch (error) {
+        console.error("DATA_API_ERROR:", error);
+        return null;
+    }
+}
